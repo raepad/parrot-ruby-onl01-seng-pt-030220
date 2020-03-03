7c5fdff2b12da578dfe02cfeda3cc9ff0e15b1e8
Rachel Padworski <rachelpadworski@gmail.com>
2020-03-03 13:08:59 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,7 +1,8 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
 def parrot(sound="Squawk!")
-  puts "#{sound}"
+  puts "Pretty bird!"
+  return "#{sound}" 
 end
 
 parrot
